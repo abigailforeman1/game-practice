224e8dcfecf93832daa5505de2cfc4dc86da4c55
abigailforeman <forea005@gmail.com>
2020-04-16 18:16:30 +0100

    added random ghost logic

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,11 @@ function init() {
   // CHALLENGES
   // placing multiple CSS classes onto the same grid square - simple fix of changing the order in CSS file
   // pacman movement storing directions from handlekeydown events and using them when possible - able to store a proposed direction in a variable and use it when pacman hits a wall but trying to find a way to check the proposed direction at every square and if it's not possible continue with current direction - a solution for this was to only update the direction variable IF pacman could move in that direction 
+  // ghost movement 
+
+  // THINGS TO ADD 
+  // - make the superfood random
+  // - sounds 
 
   // ? make the grid 
   // ? make pacman 
@@ -20,6 +25,8 @@ function init() {
   // ! if the ghosts and pacman collide GAME OVER!
   // ! if all food is eaten WINNER!
 
+  // ---------------------------------------------- VARIABLES ---------------------------------------------- //
+
   // DOM Elements 
   const grid = document.querySelector('.grid')
   const scoreDisplay = document.querySelector('.score-text')
@@ -30,22 +37,30 @@ function init() {
   const squareCount = width * width
 
   // Game variables  
+  // grid
   const squares = []
   const mazeWalls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 26, 27, 35, 36, 38, 39, 40, 41, 42, 44, 45, 47, 48, 49, 50, 51, 53, 54, 56, 57, 58, 59, 60, 62, 63, 65, 66, 67, 68, 69, 71, 72, 89, 90, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 107, 108, 114, 119, 125, 126, 127, 128, 129, 130, 132, 134, 135, 137, 139, 140, 141, 142, 143, 152, 153, 162, 163, 164, 165, 166, 168, 173, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 186, 187, 188, 189, 190, 191, 193, 194, 195, 196, 197, 198, 206, 207, 215, 216, 218, 219, 220, 222, 224, 225, 227, 229, 230, 231, 233, 234, 236, 237, 238, 240, 245, 247, 248, 249, 251, 252, 258, 259, 260, 261, 262, 263, 269, 270, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 287, 288, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323]
   const bigFoods = [19, 34, 289, 304]
+
+  // pacman info
   let pacmanIndex = 292
-  let playerScore = 0
   let direction = 'right'
-  let running = false
+  let playerScore = 0
+  // let running = false
   let startGameTimer
+
+  // ghost info 
   let redGhostMovementTimer
   let blueGhostMovementTimer
   let orangeGhostMovementTimer
   let greenGhostMovementTimer
-  const redGhost = { startIndex: 55, speed: 200 }
-  const blueGhost = { startIndex: 70, speed: 300 }
-  const greenGhost = { startIndex: 156, speed: 250 }
-  const orangeGhost = { startIndex: 149, speed: 350 }
+  const ghostDirectionOptions = [-width, width, -1, 1]
+  const redGhost = { startIndex: 55, currentIndex: 55, speed: 400, direction: width }
+  const blueGhost = { startIndex: 70, currentIndex: 70, speed: 600, direction: -1 }
+  const greenGhost = { startIndex: 156, currentIndex: 156, speed: 600, direction: 1 }
+  const orangeGhost = { startIndex: 149, currentIndex: 149, speed: 600, direction: -width }
+
+  // ---------------------------------------------- MAKING GRID ---------------------------------------------- //
 
   // * Function to create the grid
   function makeGrid() {
@@ -92,30 +107,20 @@ function init() {
 
   squares[pacmanIndex].classList.add('pacman') // controls where the player is based on the index of the square
 
+  // ---------------------------------------------- STARTING THE GAME ---------------------------------------------- //
+
   // * Function to kick off the timers for pacman movement and ghost movement
   function startGame() {
-    running = true
+    // running = true
     startGameTimer = setInterval(pacmanMovement, 300)
     redGhostMovementTimer = setInterval(redGhostMovement, redGhost.speed)
-    blueGhostMovementTimer = setInterval(blueGhostMovement, blueGhost.speed)
-    orangeGhostMovementTimer = setInterval(orangeGhostMovement, orangeGhost.speed)
-    greenGhostMovementTimer = setInterval(greenGhostMovement, greenGhost.speed)
+    // blueGhostMovementTimer = setInterval(blueGhostMovement, blueGhost.speed)
+    // orangeGhostMovementTimer = setInterval(orangeGhostMovement, orangeGhost.speed)
+    // greenGhostMovementTimer = setInterval(greenGhostMovement, greenGhost.speed)
     startBtn.classList.add('hidden')
   }
 
-  // PACMAN MOVEMENT
-
-  // declare direction variable starting with 'right'
-  // when player presses arrow key that gets pushed to direction array
-  // check if pacman can move in direction index 1
-  // splice index 0 from the array
-  // if not move in direction index 0
-
-  // set direction to right
-  // update proposedDirection based on what player presses 
-  // try and use proposedDirection 
-  // if you can use it, update direction to be the same as proposedDirection 
-  // if can't use it use direction  
+  // ---------------------------------------------- PACMAN MOVEMENT ---------------------------------------------- //
 
   // * Function to store the players chosen direction for pacman
   function updateMovement(e) {
@@ -160,7 +165,7 @@ function init() {
         }
         break
       case 'left':
-        if (!squares[pacmanIndex - 1].classList.contains('maze-wall')) { // checking it player has reached left side of the grid
+        if (!squares[pacmanIndex - 1].classList.contains('maze-wall')) { 
           pacmanIndex--
         }
         break
@@ -185,39 +190,66 @@ function init() {
     } else if (pacmanIndex === 144 && direction === 'left') {
       pacmanIndex += width
     }
+
+    if (squares[pacmanIndex].classList.contains('red-ghost')) {
+      alert('GAME OVER!')
+      // running = false
+      gameOver()
+    }
   }
+
+  // ---------------------------------------------- GHOST MOVEMENT ---------------------------------------------- //
+
+  // Ghost movement pseudocode RANDOM MOVEMENT
+
+  // remove ghost class 
+  // each interval of ghost movement use Math.random to randomly pick a direction from the 4 ghostDirectionOptions
+  // check if this direction is possible (no other ghosts in the way or maze walls)
+  // if it's possible, update the ghost current position variable and add the ghost class back on 
+  // repeat
 
   // * Function to make the red ghost move
   function redGhostMovement() {
-    squares[redGhost.startIndex].classList.remove('red-ghost')
-    redGhost.startIndex++
-    squares[redGhost.startIndex].classList.add('red-ghost')
+    squares[redGhost.currentIndex].classList.remove('red-ghost')
+    console.log(redGhost.direction)
+
+    // * checks if the next square in the ghosts movement includes a wall, if it does - update the ghosts direction 
+    if (squares[redGhost.currentIndex + redGhost.direction].classList.contains('maze-wall')) {
+      updateRedGhostDirection()
+    } else {
+      redGhost.currentIndex += redGhost.direction
+    }
+    squares[redGhost.currentIndex].classList.add('red-ghost')
+  }
+
+  // check the 4 grid squares around the ghosts current position and filter to see if they return true to not including maze wall 
+  // store the true ones in a variable 
+  // math.random those grid squares 
+
+  function updateRedGhostDirection() {
+
+    // finding the index of all 4 possible squares surrounding the ghost and putting them into an array 
+    const possibleSquareUp = redGhost.currentIndex - width
+    const possibleSquareDown = redGhost.currentIndex + width
+    const possibleSquareLeft = redGhost.currentIndex - 1
+    const possibleSquareRight = redGhost.currentIndex + 1
+    const possibleSquares = [possibleSquareUp, possibleSquareDown, possibleSquareLeft, possibleSquareRight]
+    // filtering through these squares and removing any that contain a wall
+    const availableGhostSquares = possibleSquares.filter(possibleSquare => {
+      return !squares[possibleSquare].classList.contains('maze-wall')
+    })
+    // using Math.random to pick one of the available squares indexes
+    const newIndex = availableGhostSquares[Math.floor(Math.random() * (availableGhostSquares.length))]
+    // updating the ghosts current index with the new index
+    redGhost.currentIndex = newIndex
+    // using the index of the chosen available square from the possibleSquares array and updating the ghosts direction
+    redGhost.direction = ghostDirectionOptions[possibleSquares.indexOf(newIndex)]
+
+
 
   }
 
-  // * Function to make the blue ghost move
-  function blueGhostMovement() {
-    squares[blueGhost.startIndex].classList.remove('blue-ghost')
-    blueGhost.startIndex--
-    squares[blueGhost.startIndex].classList.add('blue-ghost')
-
-  }
-
-  // * Function to make the orange ghost move
-  function orangeGhostMovement() {
-    squares[orangeGhost.startIndex].classList.remove('orange-ghost')
-    orangeGhost.startIndex++
-    squares[orangeGhost.startIndex].classList.add('orange-ghost')
-
-  }
-
-  // * Function to make the green ghost move
-  function greenGhostMovement() {
-    squares[greenGhost.startIndex].classList.remove('green-ghost')
-    greenGhost.startIndex--
-    squares[greenGhost.startIndex].classList.add('green-ghost')
-
-  }
+  // ---------------------------------------------- EATING FOOD ---------------------------------------------- //
 
   // * Function to check for pink food where pacman moves and update score
   function eatFood() {
@@ -232,8 +264,10 @@ function init() {
     }
   }
 
-  // * a Boolean check to stop pacmans and ghosts movement whenever running is set to false
-  if (running === false) {
+  // ---------------------------------------------- WIN CONDITION / GAME OVER ---------------------------------------------- //
+
+  // * Function to clear timers when player loses
+  function gameOver() {
     clearInterval(startGameTimer)
     clearInterval(redGhostMovementTimer)
     clearInterval(blueGhostMovementTimer)
